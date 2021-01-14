855f70baa8c94738645c679bbd1cfe216defbb35
Dnaja <61911770+Dnaja@users.noreply.github.com>
2021-01-13 23:43:45 -0300

    Delete Exercicios_Semana_05/Q1 directory

--- Exercicios_Semana_05/Q1/Carro.java
+++
@@ -1,9 +0,0 @@
-public class Carro extends Veiculo{
-    String modelo;
-
-    public Carro(String modelo, String placa, int ano) {
-        super(placa, ano);
-        this.modelo = modelo;
-    }
-    
-}
--- Exercicios_Semana_05/Q1/CarroEletrico.java
+++
@@ -1,9 +0,0 @@
-public class CarroEletrico extends Carro{
-    int autonomiaBateria;
-
-    public CarroEletrico(int autonomiaBateria, String modelo, String placa, int ano) {
-        super(modelo, placa, ano);
-        this.autonomiaBateria = autonomiaBateria;
-    }
-    
-}
