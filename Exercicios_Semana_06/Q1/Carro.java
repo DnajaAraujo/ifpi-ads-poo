3c0dfb653b41f281483c5487c3cd7b557036e6b8
Dnaja <61911770+Dnaja@users.noreply.github.com>
2021-01-13 23:44:13 -0300

    Add files via upload

---
+++ Exercicios_Semana_06/Q1/Carro.java
@@ -0,0 +1,9 @@
+public class Carro extends Veiculo{
+    String modelo;
+
+    public Carro(String modelo, String placa, int ano) {
+        super(placa, ano);
+        this.modelo = modelo;
+    }
+    
+}
---
+++ Exercicios_Semana_06/Q1/CarroEletrico.java
@@ -0,0 +1,9 @@
+public class CarroEletrico extends Carro{
+    int autonomiaBateria;
+
+    public CarroEletrico(int autonomiaBateria, String modelo, String placa, int ano) {
+        super(modelo, placa, ano);
+        this.autonomiaBateria = autonomiaBateria;
+    }
+    
+}
