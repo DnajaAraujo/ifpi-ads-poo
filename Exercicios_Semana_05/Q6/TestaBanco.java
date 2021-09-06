3ea82f9ce306fd8379a5cfb9265f8c64680bb3da
Dnajá Araújo <61911770+DnajaAraujo@users.noreply.github.com>
2021-09-06 19:35:29 -0300

    Update TestaBanco.java

--- Exercicios_Semana_05/Q6/TestaBanco.java
+++ Exercicios_Semana_05/Q6/TestaBanco.java
@@ -9,21 +9,21 @@ public class TestaBanco {
 
         b.inserir(c1);
         b.inserir(c2);
-		b.inserir(c3);
+	b.inserir(c3);
 
-		b.creditar("1", 100);
-		b.creditar("3", 500);
-		b.creditar("4", 200);
-		b.creditar("5", 100);
+	b.creditar("1", 100);
+	b.creditar("3", 500);
+	b.creditar("4", 200);
+	b.creditar("5", 100);
 
-		b.transferir("1", "4", 50);
-		b.transferir("2", "3", 100);
-		b.transferir("1", "5", 50);
+	b.transferir("1", "4", 50);
+	b.transferir("2", "3", 100);
+	b.transferir("1", "5", 50);
 		
-		//b.consultarIndice("2");
+	//b.consultarIndice("2");
 
-		System.out.println(b.contarContasCadastradas());
-		System.out.println(b.contarDinheiroTotal());
-		System.out.println(b.calcularMediaConta());
+	System.out.println(b.contarContasCadastradas());
+	System.out.println(b.contarDinheiroTotal());
+	System.out.println(b.calcularMediaConta());
     }
 }
